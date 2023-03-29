e8196f943af29abae73ed1ff16acb4179fd607ab
Meerwaldten <mikkell_m_j@hotmail.com>
2023-03-29 14:11:26 +0200

    Added login and option to 'log' a message

--- app.js
+++ app.js
@@ -3,41 +3,50 @@ import templateEngine from "./util/templateEngine.js";
 
 const app = express();
 
-app.use(express.static("public"))
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
 
+const login = templateEngine.readPage("./public/pages/login/login.html");
+const loginPage = templateEngine.renderPage(login, {
+    tabTitle: "Please log in"
+});
 
-const frontpage = templateEngine.readPage("./public/pages/frontpage.html");
+const frontpage = templateEngine.readPage("./public/pages/frontpage/frontpage.html");
 const frontpagePage = templateEngine.renderPage(frontpage, {
     tabTitle: "Welcome!"
 });
 
-const functions = templateEngine.readPage("./public/pages/functions.html");
+const functions = templateEngine.readPage("./public/pages/functions/functions.html");
 const functionsPage = templateEngine.renderPage(functions, {
     tabTitle: "All about functions"
 });
 
-const variables = templateEngine.readPage("./public/pages/variables.html");
+const variables = templateEngine.readPage("./public/pages/variables/variables.html");
 const variablesPage = templateEngine.renderPage(variables, {
     tabTitle: "All about variables"
 });
 
-const nodejs = templateEngine.readPage("./public/pages/nodejs.html");
+const nodejs = templateEngine.readPage("./public/pages/nodejs/nodejs.html");
 const nodejsPage = templateEngine.renderPage(nodejs, {
     tabTitle: "NodeJS is super cool"
 });
 
-const codestandards = templateEngine.readPage("./public/pages/codestandards.html");
+const codestandards = templateEngine.readPage("./public/pages/codestandards/codestandards.html");
 const codestandardsPage = templateEngine.renderPage(codestandards, {
     tabTitle: "Codestandards are very important"
 });
 
-const serving = templateEngine.readPage("./public/pages/serving.html");
+const serving = templateEngine.readPage("./public/pages/serving/serving.html");
 const servingPage = templateEngine.renderPage(serving, {
     tabTitle: "How to serve a side"
 });
 
 
 app.get("/", (req, res) => {
+    res.send(loginPage);
+});
+
+app.get("/frontpage", (req, res) => {
     res.send(frontpagePage);
 });
 
@@ -61,6 +70,16 @@ app.get("/serving", (req, res) => {
     res.send(servingPage);
 });
 
+
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    if (username === "meerwaldt" && password === "123"){
+        res.redirect("/frontpage");
+    }else {
+        res.redirect("/");
+    }
+})
 
 
 const PORT = 8080;
---
+++ public/pages/frontpage/frontpage.js
@@ -0,0 +1,4 @@
+function addMessage(){
+    const message = document.getElementById("message").value;
+    document.getElementById("displayMessage").innerText = message;
+};
